acd857732fcd40737b89cad4a3808fa8f260db87
Eduardo Borges Cambraia <139563715+duduborgesestudante@users.noreply.github.com>
2023-08-08 08:23:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function salario(){
+    var anual = Number (document.getElementById("sa").value)
+    var piso = Number (document.getElementById("psm").value)
+    var mensal = anual/12
+    document.getElementById("mensal").innerHTML = ("O salário mensal do funcionario é: "+  mensal)
+    if (mensal > piso){
+        document.getElementById("resultado").innerHTML = ("O salário do funcionario é maior que o piso!")
+    }
+    if(mensal == piso){
+        document.getElementById("resultado").innerHTML = ("O salário do funcionario é igual que o piso!")
+    }
+    if(mensal < piso){
+        document.getElementById("resultado").innerHTML = ("O salário do funcionario não é maior que o piso!")
+    }
+}
